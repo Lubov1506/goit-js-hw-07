5f0094d1138fb18aadd2643778e31b307aba40bb
Lubov Parfonova <87125430+Lubov1506@users.noreply.github.com>
2024-03-23 15:58:48 +0200

    task 2

--- js/task-1.js
+++ js/task-1.js
@@ -1,26 +1,38 @@
 const categories = document.querySelector('#categories');
-const categoriesChildren = categories.children;
+const categoriesChildren = Array.from(categories.children);
 
-function showInfo(categoriesChildren) {
-  console.log(`Number of categories: ${categoriesChildren.length}`);
-  const arr = Array.from(categoriesChildren).map(elem =>
-    Array.from(elem.children)
-  );
-  arr.forEach(el => {
-    console.log(`Category: ${el[0].innerText} ${el[1].children.length}`);
-    console.log(`Elements:  ${el[1].children.length}`);
-  });
-}
+categories.classList.add('categories');
+
+const addClassesListSubItem = categoriesChildren => {
+  categoriesChildren
+    .map(el => Array.from(el.children))
+    .flatMap(el => Array.from(el[1].children))
+    .forEach(el => {
+      el.classList.add('list-sub-item');
+    });
+};
+addClassesListSubItem(categoriesChildren);
+
+const showInfo = categoriesChildren => {
+  console.log(`Number of categories: ${categories.childElementCount}`);
+  categoriesChildren
+    .map(elem => Array.from(elem.children))
+    .forEach(el => {
+      console.log(`Category: ${el[0].textContent} ${el[1].children.length}`);
+      console.log(`Elements:  ${el[1].children.length}`);
+    });
+};
+showInfo(categoriesChildren);
+
 //or
-// function showInfo(categoriesChildren) {
+// const showInfo = categoriesChildren => {
 //   let info;
 //   info = `Number of categories: ${categoriesChildren.length}`;
-//   const arr = Array.from(categoriesChildren).map(elem =>
-//     Array.from(elem.children)
-//   );
-//   arr.forEach(el => {
-//     info += `\nCategory: ${el[0].innerText} ${el[1].children.length} \nElements:  ${el[1].children.length}`;
-//   });
+//   categoriesChildren
+//     .map(elem => Array.from(elem.children))
+//     .forEach(el => {
+//       info += `\nCategory: ${el[0].innerText} ${el[1].children.length} \nElements:  ${el[1].children.length}`;
+//     });
 //   return info;
-// }
-//console.log(showInfo(categoriesChildren));
+// };
+// console.log(showInfo(categoriesChildren));
--- js/task-2.js
+++ js/task-2.js
@@ -10,5 +10,42 @@ const images = [
   {
     url: 'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
     alt: 'Group of Horses Running',
-  }
+  },
 ];
+
+const gallery = document.querySelector('.gallery');
+gallery.classList.add('container');
+
+// const createImgs = images => {
+//   const gallery = document.querySelector('.gallery');
+//   images.forEach(item => {
+//     let li = document.createElement('li');
+//     let img = document.createElement('img');
+//     li.classList.add('gallery-item');
+//     img.setAttribute('src', item.url);
+//     img.setAttribute('alt', item.alt);
+
+//     li.appendChild(img);
+//     gallery.append(li);
+//   });
+// };
+
+// or
+const createImgs = images => {
+  const fragment = document.createDocumentFragment();
+  const imgs = images.forEach(item => {
+    let li = document.createElement('li');
+    li.classList.add('gallery-item');
+    li.insertAdjacentHTML(
+      'beforeend',
+      `
+    <img src="${item.url}" alt="${item.alt}" />
+    `
+    );
+    fragment.append(li);
+  });
+  gallery.append(fragment);
+  return imgs;
+};
+
+createImgs(images);
