17e4ad9c9ed81b166b8199e9e35b3e81a1106c38
Lubov Parfonova <87125430+Lubov1506@users.noreply.github.com>
2024-03-24 15:28:20 +0200

    task5

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,12 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+const body = document.querySelector('body');
+const btnChangeColor = document.querySelector('.change-color');
+const spanColor = document.querySelector('.color');
+
+btnChangeColor.addEventListener('click', e => {
+  const currentColor = getRandomHexColor();
+  body.style = `background-color: ${currentColor}`;
+  spanColor.textContent = `${currentColor}`;
+});
