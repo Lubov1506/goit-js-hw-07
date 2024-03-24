e47a93dd0a8e6c66248b8de428f96934e397e0af
Lubov Parfonova <87125430+Lubov1506@users.noreply.github.com>
2024-03-24 14:59:20 +0200

    task4

--- js/task-3.js
+++ js/task-3.js
@@ -1,9 +1,17 @@
 const input = document.querySelector('#name-input');
 const output = document.querySelector('#name-output');
 
-input.addEventListener('input', ({ target: { value } }) => {
+// input.addEventListener('input', ({ target: { value } }) => {
+//   let validValue = value.trim();
+//   validValue
+//     ? (output.textContent = value)
+//     : (output.textContent = 'Anonymous');
+// });
+//or
+const getValue = ({ target: { value } }) => {
   let validValue = value.trim();
   validValue
     ? (output.textContent = value)
     : (output.textContent = 'Anonymous');
-});
+};
+input.addEventListener('input', getValue);
--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,23 @@
+const form = document.querySelector('.login-form');
+const btn = document.querySelector('.login-form button');
+let data = [];
 
+const handleSubmit = e => {
+  e.preventDefault();
+  const elements = Array.from(e.target.elements);
+
+  const validEmail = strTrim(elements[0].value);
+  const validPassword = strTrim(elements[1].value);
+  if (validEmail.length === 0 || validPassword.length === 0) {
+    alert('All form fields must be filled in');
+  } else {
+    const newData = { email: validEmail, password: validPassword };
+    console.log(newData);
+    data.push(newData);
+    e.target.reset();
+  }
+};
+
+form.addEventListener('submit', e => handleSubmit(e));
+
+const strTrim = str => str.trim();
