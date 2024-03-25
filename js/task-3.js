edb8fb2d4f64bea914a883b1dfd6ed2d696b7074
Lubov Parfonova <87125430+Lubov1506@users.noreply.github.com>
2024-03-25 22:20:12 +0200

    fix validValue

--- js/task-3.js
+++ js/task-3.js
@@ -4,14 +4,14 @@ const output = document.querySelector('#name-output');
 // input.addEventListener('input', ({ target: { value } }) => {
 //   const validValue = value.trim();
 //   validValue
-//     ? (output.textContent = value)
+//     ? (output.textContent = validValue)
 //     : (output.textContent = 'Anonymous');
 // });
 //or
 const getValue = ({ target: { value } }) => {
   const validValue = value.trim();
   validValue
-    ? (output.textContent = value)
+    ? (output.textContent = validValue)
     : (output.textContent = 'Anonymous');
 };
 input.addEventListener('input', getValue);
